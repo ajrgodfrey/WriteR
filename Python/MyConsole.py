b2d86e42402e2d37cca818a7e882feed6b90b41e
marshall flax <marshall.flax@gmail.com>
2018-05-17 20:09:03 -0400

    Revert back to two-pane approach; use F8 to switch back and forth

--- Python/MyConsole.py
+++ Python/MyConsole.py
@@ -1,23 +1,29 @@
-import wx
+from wx.aui import AuiManager, AuiPaneInfo
 
 class MyConsole:
     def __init__(self, parent):
-        self.lines = []
-        self.parent = parent
+        self.console = parent.CreateTextCtrl("")
+        self.console.SetEditable(True)
+        parent._mgr.AddPane(self.console, AuiPaneInfo().Name("console")
+                          .Caption("Console").Bottom().Layer(1).Position(1).CloseButton(True)
+                          .MinimizeButton(True))
+        # parent._mgr.GetPane("console").Bottom().Layer(0).Row(0).Position(0)
+        self.console.SetValue('')
+        self.console.write('Render output goes here')
 
     def Reset(self):
-        self.lines = []
+        self.console.SetValue('')
 
     def write(self, text):
-        self.lines.append(text)
+        self.console.write(text)
 
     def CreateWriteText(self, text):
-        self.lines.append(text)
+        self.console.write(text)
+
+    def SetFocus(self):
+        self.console.SetFocus()
 
     def DoneFunc(self, retcode):
-        self.lines.append("ReturnCode: {}".format(retcode))
-        dialog = wx.MessageDialog(self.parent, "\n".join(self.lines), "CommandResult", wx.OK)
-        dialog.ShowModal()
-        dialog.Destroy()
-        self.lines = []
+        self.console.write("Done {}".format(retcode))
+        self.console.SetFocus()
 
